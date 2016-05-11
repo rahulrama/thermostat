69b91babe7ba11c949dd5871d773e0d05841ab13
rahulrama <rahulrama@gmail.com>
2016-05-11 20:03:55 +0100

    Step 5 : Power saving mode is on by default

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -28,4 +28,8 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(10)
   })
 
+  it('has power saving mode on by default', function () {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true)
+  })
+
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,6 +4,7 @@ function Thermostat(default_temp = 20) {
   this.DEFAULT_TEMPERATURE = default_temp
   this.temperature = this.DEFAULT_TEMPERATURE
   this.MINIMUM_TEMPERATURE = 10
+  this.powerSavingModeOn = true
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -23,4 +24,8 @@ Thermostat.prototype.down = function() {
 
 Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE
+}
+
+Thermostat.prototype.isPowerSavingModeOn = function() {
+  return this.powerSavingModeOn === true
 }
