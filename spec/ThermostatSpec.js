1bafded79ba013238ee0d009680f0f0865b4d803
rahulrama <rahulrama@gmail.com>
2016-05-11 20:01:11 +0100

    Step 4 : The minimum temperature is 10 degrees

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -20,4 +20,12 @@ describe('Thermostat', function() {
     thermostat.down()
     expect(thermostat.getCurrentTemperature()).toEqual(19)
   })
+
+  it('has a minimum setting of 10 degrees', function() {
+    for(var i = 0; i < 11; i++) {
+      thermostat.down()
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10)
+  })
+
 })
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,6 +3,7 @@
 function Thermostat(default_temp = 20) {
   this.DEFAULT_TEMPERATURE = default_temp
   this.temperature = this.DEFAULT_TEMPERATURE
+  this.MINIMUM_TEMPERATURE = 10
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -14,5 +15,12 @@ Thermostat.prototype.up = function() {
 }
 
 Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return
+  }
   this.temperature -= 1
+}
+
+Thermostat.prototype.isMinimumTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE
 }
