97df99db5fb53a5d9ff329806dd5ceb0a786654a
rahulrama <rahulrama@gmail.com>
2016-05-11 19:50:13 +0100

    Step 1 : Thermostat starts at 20 degrees

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Thermostat', function() {
+  var thermostat
+
+  beforeEach(function() {
+    thermostat = new Thermostat()
+  })
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20)
+  })
+
+  it('can increase the temperature with the up button', function() {
+    thermostat.up()
+    expect(thermostat.getCurrentTemperature()).toEqual(21)
+  })
+})
---
+++ src/Thermostat.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function Thermostat(default_temp = 20) {
+  this.DEFAULT_TEMPERATURE = default_temp
+  this.temperature = this.DEFAULT_TEMPERATURE
+}
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature
+}
