50403d5acb3c3c17c7740875122e8381aadfbcaf
rahulrama <rahulrama@gmail.com>
2016-05-12 17:06:04 +0100

    Step 8: APIs

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
   var thermostat = new Thermostat
+
+  $('#current-city').change(function(){
+    var city = $('#current-city').val()
+    displayWeather(city)
+  })
+
+
   $('#temperature').text(thermostat.temperature)
 
   $('#temperature-up').on('click', function() {
@@ -30,5 +37,14 @@ $(document).ready(function(){
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature())
     $('#temperature').attr('class', thermostat.energyUsage())
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed'
+    var units = '&units=metric'
+    $.get(url + token + units, function(data) {
+   $('#current-temperature').text(data.main.temp);
+   })
   }
 })
