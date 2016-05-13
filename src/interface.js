dea6c0bf215200c08d5ddaca5ceef98313e5c678
rahulrama <rahulrama@gmail.com>
2016-05-13 09:15:12 +0100

    Challenge 10 compelte - using a server to store the settings

--- src/interface.js
+++ src/interface.js
@@ -1,8 +1,30 @@
+'use strict'
+
 $(document).ready(function(){
   var thermostat = new Thermostat
+  var city
+
+  function getSettings(){
+		$.getJSON('http://localhost:4567/settings', function(data){
+		var request = data
+		thermostat.temperature = request.temperature
+		updateTemperature()
+		if(request.powerSavingModeOn) {
+      $('#power-saving-status').text('on')} else {
+      $('#power-saving-status').text('off')
+    }
+		thermostat.powerSavingModeOn = request.powerSavingModeOn
+    city = request.city
+    displayWeather(city)
+		})
+	}
+
+  getSettings()
+
+
 
   $('#current-city').change(function(){
-    var city = $('#current-city').val()
+    city = $('#current-city').val()
     displayWeather(city)
   })
 
@@ -22,21 +44,25 @@ $(document).ready(function(){
   $('#temperature-reset').on('click', function(){
     thermostat.resetTemperature()
     updateTemperature()
+    postSettings(thermostat.getCurrentTemperature(), thermostat.powerSavingModeOn, city)
   })
 
   $('#powersaving-on').on('click', function(){
     thermostat.switchPowerSavingModeOn()
     $('#power-saving-status').text('on')
+      postSettings(thermostat.getCurrentTemperature(), thermostat.powerSavingModeOn, city)
   })
 
   $('#powersaving-off').click(function(){
     thermostat.switchPowerSavingModeOff()
     $('#power-saving-status').text('off')
+    postSettings(thermostat.getCurrentTemperature(), thermostat.powerSavingModeOn, city)
   })
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature())
     $('#temperature').attr('class', thermostat.energyUsage())
+    postSettings(thermostat.getCurrentTemperature(), thermostat.powerSavingModeOn, city)
   }
 
   function displayWeather(city) {
@@ -46,5 +72,19 @@ $(document).ready(function(){
     $.get(url + token + units, function(data) {
    $('#current-temperature').text(data.main.temp);
    })
+   postSettings(thermostat.getCurrentTemperature(), thermostat.powerSavingModeOn, city)
+  }
+
+  function postSettings(temperature, powerSavingModeOn, city){
+    $.ajax ({
+  		type: "POST",
+  		url: "http://localhost:4567/settings",
+  		dataType: "json",
+  		data: JSON.stringify({"temperature":temperature,"powerSavingModeOn":powerSavingModeOn, "city":city}),
+      // contentType: "application/json; charset=utf-8",
+      failure: function(errMsg) {
+        alert(errMsg)
+      }
+    })
   }
 })
